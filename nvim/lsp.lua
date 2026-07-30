e01d80a6e4aec2a14376680c4103663c1b321a7d
Ciaran <55831661+Eldred777@users.noreply.github.com>
2026-07-30 10:39:07 +1000

    Add find usages gr keybind

--- nvim/lsp.lua
+++ nvim/lsp.lua
@@ -29,6 +29,7 @@ return {
                 local opts = { buffer = args.buf }
                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
                 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
                 vim.keymap.set("n", "<C-k>", vim.diagnostic.open_float, opts)
             end,
