693a33366bc07d0ca04aca1fabfbf78b79982b95
Ciaran <55831661+Eldred777@users.noreply.github.com>
2026-08-11 13:55:16 +1000

    nvim: format lsp.lua + add jdtls

--- nvim/lsp.lua
+++ nvim/lsp.lua
@@ -21,16 +21,20 @@ return {
             capabilities = require("blink.cmp").get_lsp_capabilities(),
         })
 
-        vim.lsp.enable({ "rust_analyzer", "pyright" })
+        vim.lsp.enable({
+            "rust_analyzer",
+            "pyright",
+            "jdtls"
+        })
 
         -- Bind LSP-related keybinds here
         vim.api.nvim_create_autocmd("LspAttach", {
             callback = function(args)
                 local opts = { buffer = args.buf }
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "gd",    vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "gD",    vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gr",    vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "K",     vim.lsp.buf.hover, opts)
                 vim.keymap.set("n", "<C-k>", vim.diagnostic.open_float, opts)
             end,
         })
