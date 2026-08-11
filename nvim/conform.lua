6a0d344ece7e5194b1eed4a3877b360922f449b7
Ciaran <55831661+Eldred777@users.noreply.github.com>
2026-08-11 13:55:01 +1000

    nvim: comment out format on write

--- nvim/conform.lua
+++ nvim/conform.lua
@@ -1,16 +1,16 @@
 -- lazy.nvim spec, loaded by plugins.lua.
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    opts = {
-        formatters_by_ft = {
-            python = { "ruff_format" },
-            rust = { "rustfmt" },
-        },
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_fallback = true,
-        },
-    },
+    -- "stevearc/conform.nvim",
+    -- event = { "BufWritePre" },
+    -- cmd = { "ConformInfo" },
+    -- opts = {
+    --     formatters_by_ft = {
+    --         python = { "ruff_format" },
+    --         rust = { "rustfmt" },
+    --     },
+    --     -- format_on_save = {
+    --     --     timeout_ms = 500,
+    --     --     lsp_fallback = true,
+    --     -- },
+    -- },
 }
